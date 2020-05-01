1ca91da5210e8de97d015696b899a8fd783d8a2f
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2020-05-02 00:21:28 +0700

    feat(view-controller): add get result and reset value func

--- Hello Dicoding/ViewController.swift
+++ Hello Dicoding/ViewController.swift
@@ -10,9 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfPanjang: UITextField!
+    @IBOutlet weak var tfLebar: UITextField!
+    @IBOutlet weak var tfResult: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        resetView()
+    }
+    
+    private func resetView(){
+        tfPanjang.text = "0"
+        tfLebar.text = "0"
+        tfResult.text = "0"
+    }
+    
+    @IBAction func getResult(_ sender: Any) {
+        
+        if let width = Double(tfLebar.text ?? "0"),
+            let height = Double(tfPanjang.text ?? "0") {
+            tfResult.text = String(width * height)
+        }else{
+            tfResult.text = "Input Salah"
+        }
+    }
+    
+    @IBAction func resetValue(_ sender: Any) {
+        resetView()
     }
     
 }
