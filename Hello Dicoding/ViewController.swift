9c4dbdd34d0a33fd33e5bac6a5d049113db64ad5
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2020-05-01 23:48:17 +0700

    first commit

---
+++ Hello Dicoding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello Dicoding
+//
+//  Created by Wahyu Permadi on 01/05/20.
+//  Copyright © 2020 Wahyu Permadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
